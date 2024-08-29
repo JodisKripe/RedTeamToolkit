d26aac1651c76073e0d79fb8bee7f53e98292d36
JodisKripe <siddharthjohri15@gmail.com>
2024-08-29 23:46:59 +0530

    small msg for dwellers

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
+$msbuild_loc=""
+
 $toolfolders = gci -Path .\Tools\
 foreach ($folder in $toolfolders){
     $any = gci -Path $folder.fullname | ? {$_.fulname -like "*.sln"}
@@ -5,6 +7,8 @@ foreach ($folder in $toolfolders){
         echo $folder.fullname
         echo $any.fullname
         #WIP
+        # This should build the .slns using msbuild.
+        # Figuring out the .Net and C# reqs is being a task rn haha.
         echo "\n"
     }
 }
