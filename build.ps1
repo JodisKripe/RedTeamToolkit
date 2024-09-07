8a3df6399b3acd75c7693ffc84111c096db908af
JodisKripe <siddharthjohri15@gmail.com>
2024-09-07 18:04:59 +0530

    build.ps1 done

--- build.ps1
+++ build.ps1
@@ -1,14 +1,32 @@
-$msbuild_loc=""
+$msbuild_loc="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+$nuget_loc="C:\Users\User\Downloads\nuget.exe"
+
+$nuget_restore="$nuget_loc restore"
+
 
 $toolfolders = gci -Path .\Tools\
 foreach ($folder in $toolfolders){
-    $any = gci -Path $folder.fullname | ? {$_.fulname -like "*.sln"}
+    $any = gci -Path $folder.fullname | ? {$_.fullname -like "*.sln"}
     if($any -ne $null){
         echo $folder.fullname
+	    cd $folder.fullname
         echo $any.fullname
+	    & $nuget_loc restore
+	    & $msbuild_loc $any.fullname
         #WIP
         # This should build the .slns using msbuild.
         # Figuring out the .Net and C# reqs is being a task rn haha.
-        echo "\n"
+    }
+    else{
+        $tool = gci -Path $folder.fullname 
+        foreach($folder in $tool){
+            $sol= gci -Path $folder | ? {$_.fullname -like "*.sln"}
+            if($sol -ne $null){
+                echo $folder.fullname
+	            cd $folder.fullname
+                & $nuget_loc restore
+	            & $msbuild_loc $sol.fullname
+            }
+        }
     }
 }
